7fade3c433fbb3b59fea05b92f07f7044d9dc8d7
kukeast <leedongguk@kakao.com>
2021-10-01 16:08:09 +0900

    location hostname 설정

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,26 +1,28 @@
 import axios from 'axios';
 
+const url = window.location.hostname
+
 export async function getToday() {
-    const response = await axios.get(`http://localhost:8080/api/today`);
+    const response = await axios.get(`http://${url}:8080/api/today`);
     return response.data;
 }
 
 export async function getTodayChart(name) {
-    const response = await axios.get(`http://localhost:8080/api/today-chart/${name}`);
+    const response = await axios.get(`http://${url}:8080/api/today-chart/${name}`);
     return response.data;
 }
 
 export async function getProducts(limit) {
-    const response = await axios.get(`http://localhost:8080/api/products/${limit}`);
+    const response = await axios.get(`http://${url}:8080/api/products/${limit}`);
     return response.data;
 }
 
 export async function getOrders(limit) {
-    const response = await axios.get(`http://localhost:8080/api/orders/${limit}`);
+    const response = await axios.get(`http://${url}:8080/api/orders/${limit}`);
     return response.data;
 }
 
 export async function getShopggu() {
-    const response = await axios.get('http://localhost:8080/api/shopggus');
+    const response = await axios.get(`http://${url}:8080/api/shopggus`);
     return response.data;
 }
