22ac8a166418080bbf395dd148a3c3c963a5ae5d
kukeast <leedongguk@kakao.com>
2021-10-06 15:31:42 +0900

    ngrok 적용

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,31 +1,32 @@
 import axios from 'axios';
 
-const url = window.location.hostname
+const url = "sellereeback.ngrok.io"
+// const url = window.location.hostname
 // const url = "localhost"
 
 export async function getToday() {
-    const response = await axios.get(`http://${url}:8080/api/today`);
+    const response = await axios.get(`https://${url}/api/today`);
     return response.data;
 }
 
 export async function getTodayChart(name) {
-    const response = await axios.get(`http://${url}:8080/api/today-chart/${name}`);
+    const response = await axios.get(`https://${url}/api/today-chart/${name}`);
     return response.data;
 }
 
 export async function getProducts(limit) {
-    const response = await axios.get(`http://${url}:8080/api/products/${limit}`);
+    const response = await axios.get(`https://${url}/api/products/${limit}`);
     return response.data;
 }
 
 export async function getOrders(limit, sortBy) {
-    const response = await axios.post(`http://${url}:8080/api/orders/${limit}`, {
+    const response = await axios.post(`https://${url}/api/orders/${limit}`, {
         sortBy : sortBy
     });
     return response.data;
 }
 
 export async function getShopggu() {
-    const response = await axios.get(`http://${url}:8080/api/shopggus`);
+    const response = await axios.get(`https://${url}/api/shopggus`);
     return response.data;
 }
