f9aba6c8febeddc3da1d21fb73f5a68b9e8b0e22
kukeast <leedongguk@kakao.com>
2021-10-18 14:53:34 +0900

    토큰 갱신 후 데이터 처리

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,46 +1,6 @@
 import axios from 'axios';
-// import { ValidToken } from './token';
 
 const Api = axios.create()
-// const config = {
-//     headers: { "Authorization": `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}` },
-// }
-// Api.interceptors.request.use(
-//     (config) => {
-// 		//AccessToken이 만료됬고, RefreshToken이 유효하다면
-//         console.log("refresh " + ValidToken("refresh-token"))
-//         console.log("access " + ValidToken("access-token"))
-// 		if(ValidToken("refresh-token") &&!ValidToken("access-token")){
-// 			// Refreshtoken을 통해 Accesstoken을 재발급한다.
-// 			refresh()
-//             .then(res => {
-//                 // Response가 정상이라면
-//                 window.localStorage.setItem("access-token", JSON.stringify(res.access_token))
-//                 window.localStorage.setItem("refresh-token", JSON.stringify(res.refresh_token))
-//                 config.headers = { "Authorization" : `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
-//                 console.log( "localstorage ", window.localStorage.getItem("access-token"))
-//                 console.log( "data ", JSON.stringify(res.access_token))
-//                 // config.header.common = {'Authorization': `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
-//                 // 실패했던 요청을 다시 수행한다.
-//                 return config
-//             })
-// 		} else if (!ValidToken("refresh-token")){
-// 			localStorage.clear();
-//             const history = useHistory()
-//             history.push("/login")
-// 		}
-//         // config.header.common = {'Authorization': `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
-//         // config.header.Authorization = `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`
-//         // config.header = { 'Authorization': `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
-//         // config.headers = { "Authorization" : `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
-//         return config
-//     },
-//     (err) => {
-//         return Promise.reject(err);
-//     }
-// );
-
-
 Api.interceptors.request.use(
     (config) => {
         config.headers = { "Authorization" : `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
@@ -55,24 +15,18 @@ Api.interceptors.response.use(
         return config
     },
     (err) => {
-        // console.log(err.response.data)
-        // console.log("refresh " + ValidToken("refresh-token"))
-        // console.log("access " + ValidToken("access-token"))
         if(err.response.data === "Token is expired"){
-            refresh()
+            return refresh()
             .then(res => {
                 localStorage.setItem("access-token", JSON.stringify(res.access_token))
                 localStorage.setItem("refresh-token", JSON.stringify(res.refresh_token))
                 err.config.headers = { "Authorization" : `Bearer ${JSON.parse(window.localStorage.getItem("access-token"))}`}
-                // console.log(JSON.parse(window.localStorage.getItem("access-token")))
-                window.location.reload()
-                //return axios(err.config)
+                return axios(err.config)
             })
         }
         return Promise.reject(err);
     }
-);
-
+)
 
 const url = "sellereeback.ngrok.io"
 
