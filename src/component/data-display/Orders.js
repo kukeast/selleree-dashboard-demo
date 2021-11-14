cb9b64d2f6088ccddf8409bda819d7c8f305d31e
kukeast <leedongguk@kakao.com>
2021-11-14 23:26:04 +0900

    주문 조회 단위 추가

--- src/component/data-display/Orders.js
+++ src/component/data-display/Orders.js
@@ -10,6 +10,8 @@ import { COLOR } from '../../constants/color';
 import TextButton from '../inputs/TextButton';
 import SkeletonOrder from './SkeletonOrder';
 import OrderStack from './OrderStack';
+import Select from '../inputs/Select';
+import useLocalStorage from '../../util/useLocalStorage';
 
 const Wrapper = styled.div`
     display: flex;
@@ -20,7 +22,17 @@ const Wrapper = styled.div`
     overflow-x: scroll;
 `
 const SortWrapper = styled.div`
-    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    > div{
+        flex: 1;
+    }
+    > div:nth-child(2){
+        text-align: center;
+    }
+    > div:nth-child(3){
+        text-align: right;
+    }
 `
 const ButtonWrapper = styled.div`
     margin-top: 20px;
@@ -37,13 +49,18 @@ const DateWrapper = styled.div`
     background-color: ${COLOR.gray1};
     border-radius: 8px;
 `
+const options = {
+    20: "20개씩 보기",
+    40: "40개씩 보기",
+    60: "60개씩 보기",
+}
 
 function Orders () {
-    const [limit, setLimit] = useState(20)
+    const [unit, setUnit] = useLocalStorage("order_unit", 20)
+    const [limit, setLimit] = useState(parseInt(unit))
     const [sortBy, setSortBy] = useState("created_at")
     const [orderList, setOrderList] = useState([])
     const [response] = useAsync(() => getOrders(limit, sortBy),[limit, sortBy])
-    
     useEffect(() => {
         if(response.data){
             const newArr = []
@@ -71,6 +88,12 @@ function Orders () {
         }
     }, [response, sortBy])
 
+    useEffect(() => {
+        if(limit < parseInt(unit)){
+            setLimit(parseInt(unit))
+        }
+    }, [unit, limit])
+
     const skeleton = () => {
         const result = [];
         for (let i = 0; i < 16; i++) {
@@ -83,12 +106,23 @@ function Orders () {
         setOrderList([])
         setSortBy(prev => prev === "created_at" ? "last_modified_at" : "created_at")
     }
+
+    const SelectCallback = value => {
+        setUnit(value)
+    }
+    
     return(
         <>
             <SortWrapper>
-                <TextButton icon='sort' onClick={switchOrderSort}>
-                    {sortBy === "created_at" ? "최근 생성 순" : "최근 업데이트 순"}
-                </TextButton>
+                <div></div>
+                <div>
+                    <TextButton icon='sort' onClick={switchOrderSort}>
+                        {sortBy === "created_at" ? "최근 생성 순" : "최근 업데이트 순"}
+                    </TextButton>
+                </div>
+                <div>
+                    <Select options={options} defaultValue={limit} callback={SelectCallback}/>
+                </div>
             </SortWrapper>
             <Wrapper>
                 <OrdersHeader sortBy={sortBy}/>
@@ -111,7 +145,7 @@ function Orders () {
                 )): skeleton()}
             </Wrapper>
             <ButtonWrapper>
-                <Button type="mono" onClick={() => setLimit(prev => prev + 20)} isLoading={response.loading}>20개 더 보기</Button>
+                <Button type="mono" onClick={() => setLimit(prev => prev + parseInt(unit))} isLoading={response.loading}>{unit}개 더 보기</Button>
             </ButtonWrapper>
         </>
     )
