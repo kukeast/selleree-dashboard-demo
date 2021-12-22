ef29e2d02e77a73a8ff4b8fdbbd06a5a16fe399e
kukeast <leedongguk@kakao.com>
2021-12-22 23:55:05 +0900

    router 변경

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter basename="/selleree-dashboard-demo">
+  <HashRouter basename="/selleree-dashboard-demo">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
